3728166a00448c688bcc3a2da49a9c37cf9741de
sakura <sakura@sakura.local>
2018-01-18 21:54:26 +0900

    first commit

---
+++ PasswordVisibleExample/JJPasswordTextField.h
@@ -0,0 +1,13 @@
+//
+//  JJPasswordTextField.h
+//  PasswordVisibleExample
+//
+//  Created by sakura on 2018/01/15.
+//  Copyright © 2018年 Keisuke Okamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJPasswordTextField : UITextField
+
+@end
---
+++ PasswordVisibleExample/JJPasswordTextField.m
@@ -0,0 +1,21 @@
+//
+//  JJPasswordTextField.m
+//  PasswordVisibleExample
+//
+//  Created by sakura on 2018/01/15.
+//  Copyright © 2018年 Keisuke Okamoto. All rights reserved.
+//
+
+#import "JJPasswordTextField.h"
+
+@implementation JJPasswordTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
